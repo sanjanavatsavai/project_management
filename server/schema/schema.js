cb98636e40a0064bd0d17f08d0f04d675a871c0d
sanjanavatsavai <sanju82@outlook.com>
2025-09-23 14:18:06 -0400

    created schema to fetch projects data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,26 @@ const { projects, clients} = require('../sampleData.js')
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList  } = require('graphql');
 
 
+//project type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID }, 
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client => client.id === parent.clientId);
+            }
+        }
+    }),
+
+});
+
+
+
 //client type
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -20,6 +40,22 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+
+         projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            },
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id);
+
+            }
+        },
+
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
